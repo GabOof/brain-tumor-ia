334e2074a980ab7c69ac1e1c7f149c7ae54f0b6b
GabOof <oliveirafonseca.gab@gmail.com>
2026-06-16 19:52:41 -0300

    refactor(features.py): update image loading function to return normalized array

--- src/features.py
+++ src/features.py
@@ -6,7 +6,8 @@ from PIL import Image
 def carregar_imagem_cinza(caminho: Path, img_size: int) -> np.ndarray:
     imagem = Image.open(caminho).convert("L")
     imagem = imagem.resize((img_size, img_size))
-    return imagem
+    array = np.asarray(imagem, dtype=np.float32) / 255.0
+    return array
 
 def vetorizar_imagem():
     # TODO: Implementar função para vetorizar imagem
