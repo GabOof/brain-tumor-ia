bbd658a413ae4447ec81897a4b9ed76a0e3bea4b
GabOof <oliveirafonseca.gab@gmail.com>
2026-06-16 19:50:40 -0300

    feat(features.py): add initial feature functions for image processing

---
+++ src/features.py
@@ -0,0 +1,7 @@
+def carregar_imagem_cinza():
+    # TODO: Implementar função para carregar imagem em escala de cinza
+    pass
+
+def vetorizar_imagem():
+    # TODO: Implementar função para vetorizar imagem
+    pass
